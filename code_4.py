4c8418c3acc9bdf9ab69b1c92d81cd32a6c18895
markmg13 <50847192+markmg13@users.noreply.github.com>
2024-02-16 19:39:15 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -9,7 +9,7 @@ def get_big_mac_price_by_year(year,country_code):
                'usa', 'zaf', 'are', 'aze', 'bhr', 'col', 'cri', 'egy', 'gtm', 'hon', 'hrv', 'jor', 'kwt', 
                'lbn', 'lka', 'mda', 'nic', 'nor', 'omn', 'pak', 'per', 'phl', 'qat', 'rou', 'sau', 'tur', 'ury', 'ven', 'vnm']
     if country_code.lower() in c_codes:
-        query_data = df[(df['iso_a3'].str.lower()== country_code.lower()) & (df['date'].str.startswith(str(year)))]
+        query_data = df[(df['iso_a3'].str.lower() == country_code.lower()) & (df['date'].str.startswith(str(year)))]
         round_a = round(query_data['dollar_price'].mean(),2)
     
     return round_a
@@ -20,7 +20,7 @@ def get_big_mac_price_by_country(country_code):
                'usa', 'zaf', 'are', 'aze', 'bhr', 'col', 'cri', 'egy', 'gtm', 'hon', 'hrv', 'jor', 'kwt', 
                'lbn', 'lka', 'mda', 'nic', 'nor', 'omn', 'pak', 'per', 'phl', 'qat', 'rou', 'sau', 'tur', 'ury', 'ven', 'vnm']
     if country_code.lower() in c_codes:
-        query_data = df[(df['iso_a3'].str.lower()== country_code.lower()) & (df['name'].str.startswith(str(country_code)))]
+        query_data = df[(df['iso_a3'].str.lower() == country_code.lower()) & (df['name'].str.startswith(str(country_code)))]
         round_b = round(query_data['dollar_price'].mean(),2)
     
     return round_b
